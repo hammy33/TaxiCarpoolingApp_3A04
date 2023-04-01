c07f068a64c5e80a8895d1ce15c572677ed8cc38
Anton Kanugalawattage <antondilon@gmail.com>
2023-04-01 18:50:02 -0400

    Add get + post requests

--- backend/app.py
+++ backend/app.py
@@ -4,12 +4,14 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
-DEG_TO_METERS = 111139
-LOC_TOLERANCE = 1000 # meters
-
 @app.route("/")
 def home():
     return "Hello, World!"
+
+DEG_TO_METERS = 111139
+LOC_TOLERANCE = 1000 # meters
+
+### ------ STATES ------ ###
 
 # Offer : {email, startCord: {long: float, lat: float}, endCord: {long: float, lat: float}}
 offers = [
@@ -20,11 +22,14 @@ offers = [
     }
 ]
 
-# Request : {email, startCord: {long: float, lat: float}, endCord: {long: float, lat: float}}
+# Request : {requester, offerer}
 requests = []
 
 # Request : {carpoolers: [email], startCord: float, endCord: float}
 carpools = []
+
+
+### ------ OFFERS ------ ###
 
 # Gets active carpool offers given start and end cords
 # BODY: {startCord: {long, lat}, endCord: {long, lat}}
@@ -47,6 +52,27 @@ def getOffers():
 @app.route("/offers", methods = ['POST'])
 def addOffers():
     offers.append(request.get_json())
+    return jsonify(success=True)
+
+
+### ------ REQUESTS ------ ###
+
+# Gets requests made to offers for an offerer
+# /requests/offerer@test.com
+@app.route("/requests/<offerer>")
+def getRequests(offerer):
+    offerRequests = []
+    for request in requests:
+        if request['offerer'] == offerer:
+            offerRequests.append(request)
+
+    return jsonify(offerRequests)
+
+# Adds requests 
+# BODY: {requester:email, offerer:email}
+@app.route("/requests", methods = ['POST'])
+def addRequests():
+    requests.append(request.get_json())
     return jsonify(success=True)
 
 if __name__ == "__main__":
