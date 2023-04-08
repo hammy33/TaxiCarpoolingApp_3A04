f0d7b8b9027c0a52e43ef005720fade8cfa7945f
Hamrish Saravanakumar <saravah@mcmaster.ca>
2023-04-07 21:54:52 -0400

    made change in app js for qrscanner js

--- CarpoolApp/App.js
+++ CarpoolApp/App.js
@@ -1,21 +1,23 @@
 import * as React from 'react';
-import { Text, View, Button } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import HomeScreen from './components/Home';
 import Login from './components/Login';
 import OfferCarpool from './components/OfferCarpool';
+import QRScanner from './components/QRScanner.js';
+
 
 const Stack = createNativeStackNavigator();
 
 const MyStack = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer initialRouteName="Welcome">
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{title: 'Welcome to our carpool app'}}/>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="OfferCarpool" component={OfferCarpool}/>
+        <Stack.Screen name="Home" component={HomeScreen} options={navigationOptions={headerShown: false}}/>
+        <Stack.Screen name="Login" component={Login} options={navigationOptions={headerShown: false}}/>
+        <Stack.Screen name="OfferCarpool" component={OfferCarpool} options={navigationOptions={headerShown: false}}/>
+        <Stack.Screen name="QRScanner" component={QRScanner} options={navigationOptions={headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
