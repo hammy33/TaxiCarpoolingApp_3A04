254be99b2ad7fd6456007122065180dc18349614
Hamrish Saravanakumar <saravah@mcmaster.ca>
2023-04-11 21:33:11 -0400

    login final formatting

--- CarpoolApp/components/Login.js
+++ CarpoolApp/components/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Alert, View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import styles from '../styles/styles';
 import DataService from '../DataService';
 
 export default function Login({ navigation }) {
@@ -22,34 +21,75 @@ export default function Login({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
-        <View style={[styles.inputContainer, { flex: 1 }]}>
-          <TextInput
-            style={[styles.input, { flex: 1 }]}
-            multiline={true} 
-            placeholder="Email"
-            placeholderTextColor="#aaaaaa"
-            onChangeText={(text) => setEmail(text)}
-            value={email}
-            autoCapitalize="none"
-            autoCompleteType="email"
-            keyboardType="email-address"
-          />
-        </View>
-        
-        <View style={[styles.inputContainer, { flex: 1 }]}>
-          <TextInput
-            style={[styles.input, { flex: 1 }]}
-            multiline={true} 
-            placeholder="Password"
-            placeholderTextColor="#aaaaaa"
-            onChangeText={(text) => setPassword(text)}
-            value={password}
-            secureTextEntry={true}
-          />
-        </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          placeholderTextColor="#aaaaaa"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          autoCapitalize="none"
+          autoCompleteType="email"
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          placeholderTextColor="#aaaaaa"
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          secureTextEntry={true}
+        />
+      </View>
       <TouchableOpacity style={styles.button} onPress={handleSubmission}>
         <Text style={styles.buttonText}>Submit</Text>
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#444140'
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    alignSelf: 'center',
+    color: '#F7EBE8',
+  },
+  inputContainer: {
+    marginBottom: 30,
+  },
+  input: {
+    width: 250,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#1E1E24',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    alignSelf: 'center',
+    marginBottom: 20,
+    minWidth: 200,
+    borderWidth: 1,
+    borderColor: '#FFF',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+});
